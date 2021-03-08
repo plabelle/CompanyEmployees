6df06e438503ee2978913391300eb8b2e94098e2
Paul <labellepaul@outlook.com>
2021-03-08 12:30:12 -0500

    added initial Db configuration

---
+++ Entities/Configuration/EmtityTypeBuilder.cs
@@ -0,0 +1,6 @@
+﻿namespace Entities.Configuration
+{
+    public class EntityTypeBuilder
+    {
+    }
+}
--- Entities/RepositoryContext.cs
+++ Entities/RepositoryContext.cs
@@ -1,4 +1,5 @@
-﻿using Entities.Models;
+﻿using Entities.Configuration;
+using Entities.Models;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -11,6 +12,12 @@ namespace Entities
     public class RepositoryContext : DbContext
     {
         public RepositoryContext(DbContextOptions options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new CompanyConfiguration());
+            modelBuilder.ApplyConfiguration(new EmployeeConfiguration());
+        }
 
         public DbSet<Company> Companies { get; set; }
         public DbSet<Employee> Employees { get; set; }
